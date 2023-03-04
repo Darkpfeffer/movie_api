f36314fc389baa662a400a47ca331e94341a6e2a
Darkpfeffer <darkpfeffer97@gmail.com>
2023-03-04 18:17:26 +0100

    added function (remove user by id)

--- index.js
+++ index.js
@@ -283,7 +283,7 @@ app.post('/users/:id/:movieTitle', (req, res) => {
 
     if ( user) {
         user.favoriteMovies.push(movieTitle);
-        res.status(200).json(`${movieTitle} has been added to user ${id}'s array`);
+        res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
     } else{
         res.status(400).send('User not found')
     }
@@ -366,7 +366,21 @@ app.delete('/users/:id/:movieTitle', (req, res) => {
 
     if ( user) {
         user.favoriteMovies = user.favoriteMovies.filter( title=> title!== movieTitle)
-        res.status(200).json(`${movieTitle} has been removed from user ${id}'s array`);
+        res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
+    } else{
+        res.status(400).send('User not found')
+    }
+})
+
+// Allow users to deregister
+app.delete('/users/:id', (req, res) => {
+    const{ id}= req.params;
+
+    let user= users.find( user=> user.id== id);
+
+    if ( user) {
+        users = users.filter( user=> user.id!= id)
+        res.status(200).send(`user ${id} has been deleted`);
     } else{
         res.status(400).send('User not found')
     }
