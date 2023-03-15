1cf14e2219e211344a3e5592807e69dcac796f29
Darkpfeffer <darkpfeffer97@gmail.com>
2023-03-15 10:41:52 +0100

    added schema for Users

--- models.js
+++ models.js
@@ -1,5 +1,6 @@
 const mongoose= require('mongoose');
 
+// Schema for movies
 let movieSchema= mongoose.Schema({
     Title: {type: String, required: true},
     Description: {type: String, required: true},
@@ -15,3 +16,18 @@ let movieSchema= mongoose.Schema({
     ImagePath: String,
     Featured: Boolean
 });
+
+// Schema for users
+let userSchema= mongoose.Schema({
+    Username: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: Date,
+    FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, red: 'Movie'}]
+});
+
+let Movie= mongoose.model('Movie', movieSchema);
+let User= mongoose.model('User', userSchema);
+
+module.exports.Movie= Movie;
+module.exports.User= User;
