2cbf332934a5b187094d089381472c4328f5d52d
Darkpfeffer <darkpfeffer97@gmail.com>
2023-03-23 10:26:30 +0100

    added password hashing to update user info

--- index.js
+++ index.js
@@ -194,11 +194,12 @@ app.get('/movies/directors/:directorName', passport.authenticate('jwt', {session
     (required)
 } */
 app.put('/users/:Username', passport.authenticate('jwt', {session: false }), (req, res) => {
+    let hashedPassword= Users.hashPassword(req.body.Password)
 
     Users.findOneAndUpdate({Username: req.params.Username }, { $set:
             {
                 Username: req.body.Username,
-                Password: req.body.Password,
+                Password: hashedPassword,
                 Email: req.body.Email,
                 Birthday: req.body.Birthday
             }
