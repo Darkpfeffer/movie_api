d77e9e1ab8f617e7b04226f1658ba4e056797936
Darkpfeffer <darkpfeffer97@gmail.com>
2023-04-12 13:22:46 +0200

    remove authentication at '/movies' endpoint

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ app.get('/', (req,res) => {
 })
 
 // READ all movies
-app.get('/movies', passport.authenticate('jwt', {session: false }), (req,res) => {
+app.get('/movies', (req,res) => {
     Movies.find()
         .then((movies) => {
             res.status(200).json(movies)
